9150e73d35718d50b5441d8ae004c382c945c6b5
zeph180 <zephrichards1@gmail.com>
2025-08-06 22:27:52 +0300

    Setup basic project structure

---
+++ src/app/amenities/amenities.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-amenities',
+  imports: [],
+  templateUrl: './amenities.html',
+  styleUrl: './amenities.css'
+})
+export class Amenities {
+
+}
