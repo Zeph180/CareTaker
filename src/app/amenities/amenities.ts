84e2e3a10b43eb0ca64da64e789371ab5b8cfd32
emmailakut256 <emmailakut@gmail.com>
2025-08-10 18:41:42 +0300

    added amenities

--- src/app/amenities/amenities.ts
+++ src/app/amenities/amenities.ts
@@ -8,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class Amenities {
 
+    indoorActivities = [
+    {
+      title: 'Private Rooms',
+      description: 'Lorem ipsum dolor sit amet consectetur. Augue non molestudis placerat faucibus nam purus sem. Urna purus porttitor dignissim congue pellentesque ac hac.',
+      image: 'privateRooms.png'
+    },
+    {
+      title: 'Semi-Private Rooms',
+      description: 'Lorem ipsum dolor sit amet consectetur. Augue non molestudis placerat faucibus nam purus sem. Urna purus porttitor dignissim congue pellentesque ac hac.',
+      image: 'semiPrivateRooms.png'
+    },
+  ];
+
+    outdoorActivities = [
+    {
+      title: 'outdoorSpaces',
+      description: 'Lorem ipsum dolor sit amet consectetur. Augue non molestudis placerat faucibus nam purus sem. Urna purus porttitor dignissim congue pellentesque ac hac.',
+      image: 'outdoorSpaces.png'
+    },
+    {
+      title: 'gardens',
+      description: 'Lorem ipsum dolor sit amet consectetur. Augue non molestudis placerat faucibus nam purus sem. Urna purus porttitor dignissim congue pellentesque ac hac.',
+      image: 'gardens.jpg'
+    },
+  ];
+
+    commonAreas = [
+    {
+      title: 'outdoorSpaces',
+      description: 'Lorem ipsum dolor sit amet consectetur. Augue non molestudis placerat faucibus nam purus sem. Urna purus porttitor dignissim congue pellentesque ac hac.',
+      image: 'common1.png'
+    },
+    {
+      title: 'gardens',
+      description: 'Lorem ipsum dolor sit amet consectetur. Augue non molestudis placerat faucibus nam purus sem. Urna purus porttitor dignissim congue pellentesque ac hac.',
+      image: 'common2.png'
+    },
+  ];
+
+  accessibility = [
+    {
+      icon: 'houseIcon.svg',
+      title: 'Accessibility Features #1',
+      description: 'Personalized daily assistance in a warm, homelike environment. From grooming to companionship, we ensure every resident feels valued and cared for.'
+    },
+    {
+      icon: 'houseIcon.svg',
+      title: 'Accessibility Features #2',
+      description: 'Nutritious, chef-prepared meals tailored to individual health needs, promoting wellness, energy, and enjoyment at every mealtime.'
+    },
+    {
+      icon: 'houseIcon.svg',
+      title: 'Accessibility Features #3',
+      description: 'Professional medical support delivered with compassion, ensuring residents receive expert care for both short-term recovery and long-term health.'
+    }
+  ];
 }
